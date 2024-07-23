16559fde57acbdb32420bc688d5cc59d381fdced
Matt Venables <matt@venabl.es>
2024-07-22 22:42:06 -0400

    Add favicons

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,11 +11,11 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#fff",
     theme_color: "#fff",
     icons: [
-      {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon"
-      }
+      { src: "/favicon.ico", sizes: "any", type: "image/x-icon" },
+      { src: "/icon1.png", sizes: "16x16", type: "image/png" },
+      { src: "/icon2.png", sizes: "32x32", type: "image/png" },
+      { src: "/icon3.png", sizes: "192x192", type: "image/png" },
+      { src: "/icon4.png", sizes: "512x512", type: "image/png" }
     ]
   }
 }
---
+++ components/icons/brand/rocket.tsx
@@ -0,0 +1,8 @@
+import { RocketIcon } from "lucide-react"
+import type { LucideProps } from "lucide-react"
+
+type LogoProps = LucideProps
+
+export function Logo(props: LogoProps) {
+  return <RocketIcon {...props} />
+}
