da84d219fb0f87f86661cc47c6568de94437a8d8
Matt Venables <matt@venabl.es>
2024-09-13 04:11:15 -0400

    Fix fullURL test for readonly env vars

--- lib/url-fns/full-url.test.ts
+++ lib/url-fns/full-url.test.ts
@@ -1,18 +1,9 @@
 import { describe, expect, test } from "bun:test"
 
-import { env } from "@/env/client"
-
 import { fullURL } from "./full-url"
 
 describe("fullURL()", () => {
   test("returns a full URL by appending the path to the host", () => {
-    const oldHost = env.NEXT_PUBLIC_HOST
-
-    // @ts-expect-error - env vars are readonly
-    env.NEXT_PUBLIC_HOST = "https://example.com"
-    expect(fullURL("/path").toString()).toBe("https://example.com/path")
-
-    // @ts-expect-error - env vars are readonly
-    env.NEXT_PUBLIC_HOST = oldHost
+    expect(fullURL("/path").toString()).toBe("http://test.host/path")
   })
 })
