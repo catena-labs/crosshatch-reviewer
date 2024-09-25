459e000f8359ef24d705a06588d6e0db7b393452
Matt Venables <matt@venabl.es>
2024-09-25 13:39:47 -0400

    Move legal page to root

---
+++ app/legal/layout.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import type { PropsWithChildren } from "react"
+import { Footer } from "@/components/layout/footer"
+import { Header } from "@/components/layout/header"
+import { Button } from "@/components/ui/button"
+import { siteConfig } from "@/config/site"
+
+export default function LegalLayout({ children }: Readonly<PropsWithChildren>) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="container prose prose-zinc py-8 dark:prose-invert lg:prose-xl">
+        <Button className="px-0 no-underline" variant="link" asChild>
+          <Link href="/">← Back to {siteConfig.name}</Link>
+        </Button>
+        <article className="">{children}</article>
+      </main>
+      <Footer />
+    </div>
+  )
+}
