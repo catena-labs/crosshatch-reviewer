3c28b4a1e0d82ae4a0013fa8e5bb0780a5c2c4d0
Dominic Bosco <dominic@catena.xyz>
2024-11-04 13:59:45 -0500

    [CRO-694] Use PR title+description when generating summary

--- lib/crosshatch/summarize-changes.ts
+++ lib/crosshatch/summarize-changes.ts
@@ -2,7 +2,12 @@ import { OpenAI } from "openai"
 import { env } from "@/env/server"
 
 const SYSTEM_PROMPT = `You are a code reviewer assistant who summarizes code changes and flags potential bugs and security threats.
-You will be given a diff of a pull request. You will summarize the changes into 4 sections:
+You will be given three inputs:
+1. A pull request title that briefly describes the change
+2. A pull request description that provides context about the changes
+3. A diff of the pull request changes
+
+Using all inputs, you will summarize the changes into 4 sections:
 The first section is 'Summary', which is a maximum of 3 sentences giving an overview of the Pull Request and alerting if there is any major issues.
 The second section is 'Notable Changes' which is a bulleted list of major and notable changes.
 The third section is 'Potential concerns', which is a bulleted list any bugs, security threats, or other concerns if any are present.
@@ -12,7 +17,11 @@ For all sections: never mention unimportant changes, such as imports or formatti
 /**
  * TODO: Limit the number of changes!
  */
-export async function summarizeChanges(changes: string[]) {
+export async function summarizeChanges(
+  title: string,
+  description: string | null,
+  changes: string[]
+) {
   if (!changes.length) {
     return
   }
@@ -21,6 +30,13 @@ export async function summarizeChanges(changes: string[]) {
     baseURL: env.CROSSHATCH_BASE_URL,
     apiKey: env.CROSSHATCH_API_KEY
   })
+
+  const userMessage = `
+  TITLE: ${title}
+  DESCRIPTION: ${description ?? "No description provided"}
+  CHANGES:
+  ${changes.join("\n\n")}
+  `
 
   const response = await openai.chat.completions.create({
     stream: false,
@@ -32,7 +48,7 @@ export async function summarizeChanges(changes: string[]) {
       },
       {
         role: "user",
-        content: `Here is the diff:\n${changes.join("\n\n")}`
+        content: userMessage
       }
     ]
   })
